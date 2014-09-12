37cfcb5d7904221b7b8b9aba60fbbad3c272ce11
Jack Feser <jack.feser@gmail.com>
2014-09-12 01:22:52 -0500

    Add an optional recursion limit to eval.

--- src/eval.ml
+++ src/eval.ml
@@ -60,108 +60,109 @@ let (stdlib_vctx: value Ctx.t) =
                      Ctx.bind ctx name value)
 
 (** Evaluate an expression in the provided context. *)
-let eval ctx expr : value =
+let eval ?recursion_limit:(limit = (-1)) ctx expr : value =
   let ctx' =
     Ctx.merge stdlib_vctx ctx ~f:(fun ~key:_ value ->
                                   match value with
                                   | `Both (_, v) | `Left v | `Right v -> Some v) in
-  let rec ev ctx expr : value =
-    let ev_all = List.map ~f:(ev ctx) in
-    match expr with
-    | `Num x  -> `Num x
-    | `Bool x -> `Bool x
-    | `List x -> `List (ev_all x)
-    | `Tree x -> `Tree (Tree.map x ~f:(ev ctx))
-    | `Id id  -> Ctx.lookup_exn ctx id
-    | `Let (name, bound, body) -> 
-       let ctx' = Ctx.bind ctx name `Unit in
-       Ctx.update ctx' name (ev ctx' bound);
-       ev ctx' body
-    | `Lambda _ as lambda -> `Closure (lambda, ctx)
-    | `Apply (func, args) ->
-       (match ev ctx func with
-        | `Closure (`Lambda (arg_names, body), enclosed_ctx) ->
-           (match List.zip arg_names (ev_all args) with
-            | Some bindings ->
-               let ctx' = List.fold bindings
-                                    ~init:(enclosed_ctx)
-                                    ~f:(fun ctx' (arg_name, value) -> Ctx.bind ctx' arg_name value) in
-               ev ctx' body
-            | None -> argn_error @@ expr_to_string body)
-        | _ -> raise @@ RuntimeError (sprintf "Tried to apply a non-function: %s" (expr_to_string expr)))
-    | `Op (op, args) ->
-       let open Op in
-       (match op with
-        | Not -> (match ev_all args with
-                  | [`Bool x] -> `Bool (not x)
-                  | _ -> arg_error op args)
-        | Car -> (match ev_all args with
-                  | [`List (x::_)] -> x
-                  | _ -> arg_error op args)
-        | Cdr -> (match ev_all args with
-                  | [`List (_::xs)] -> `List xs
-                  | _ -> arg_error op args)
-        | Plus -> (match ev_all args with
-                   | [`Num x; `Num y] -> `Num (x + y)
-                   | _ -> arg_error op args)
-        | Minus -> (match ev_all args with
-                    | [`Num x; `Num y] -> `Num (x - y)
+  let rec ev ctx lim expr : value =
+    if lim = 0 then raise (RuntimeError "Exceeded recursion limit.") else
+      let ev_all = List.map ~f:(ev ctx lim) in
+      match expr with
+      | `Num x  -> `Num x
+      | `Bool x -> `Bool x
+      | `List x -> `List (ev_all x)
+      | `Tree x -> `Tree (Tree.map x ~f:(ev ctx lim))
+      | `Id id  -> Ctx.lookup_exn ctx id
+      | `Let (name, bound, body) -> 
+         let ctx' = Ctx.bind ctx name `Unit in
+         Ctx.update ctx' name (ev ctx' lim bound);
+         ev ctx' lim body
+      | `Lambda _ as lambda -> `Closure (lambda, ctx)
+      | `Apply (func, args) ->
+         (match ev ctx lim func with
+          | `Closure (`Lambda (arg_names, body), enclosed_ctx) ->
+             (match List.zip arg_names (ev_all args) with
+              | Some bindings ->
+                 let ctx' = List.fold bindings
+                                      ~init:(enclosed_ctx)
+                                      ~f:(fun ctx' (arg_name, value) -> Ctx.bind ctx' arg_name value) in
+                 ev ctx' (lim - 1) body
+              | None -> argn_error @@ expr_to_string body)
+          | _ -> raise @@ RuntimeError (sprintf "Tried to apply a non-function: %s" (expr_to_string expr)))
+      | `Op (op, args) ->
+         let open Op in
+         (match op with
+          | Not -> (match ev_all args with
+                    | [`Bool x] -> `Bool (not x)
                     | _ -> arg_error op args)
-        | Mul -> (match ev_all args with
-                  | [`Num x; `Num y] -> `Num (x * y)
-                  | _ -> arg_error op args)
-        | Div -> (match ev_all args with
-                  | [`Num x; `Num y] -> if y = 0 then raise (RuntimeError "Divide by zero.")
-                                        else `Num (x / y)
-                  | _ -> arg_error op args)
-        | Mod -> (match ev_all args with
-                  | [`Num x; `Num y] -> if y = 0 then raise (RuntimeError "Divide by zero.")
-                                        else `Num (x mod y)
-                  | _ -> arg_error op args)
-        | Eq -> (match ev_all args with
-                 | [x; y] -> `Bool (x = y)
-                 | _ -> arg_error op args)
-        | Neq -> (match ev_all args with
-                  | [x; y] -> `Bool (x <> y)
-                  | _ -> arg_error op args)
-        | Lt -> (match ev_all args with
-                 | [`Num x; `Num y] -> `Bool (x < y)
-                 | _ -> arg_error op args)
-        | Leq -> (match ev_all args with
-                  | [`Num x; `Num y] -> `Bool (x <= y)
-                  | _ -> arg_error op args)
-        | Gt -> (match ev_all args with
-                 | [`Num x; `Num y] -> `Bool (x > y)
-                 | _ -> arg_error op args)
-        | Geq -> (match ev_all args with
-                  | [`Num x; `Num y] -> `Bool (x >= y)
-                  | _ -> arg_error op args)
-        | And -> (match ev_all args with
-                  | [`Bool x; `Bool y] -> `Bool (x && y)
-                  | _ -> arg_error op args)
-        | Or -> (match ev_all args with
-                 | [`Bool x; `Bool y] -> `Bool (x || y)
-                 | _ -> arg_error op args)
-        | Cons -> (match ev_all args with
-                   | [x; `List y] -> `List (x :: y)
-                   | _ -> arg_error op args)
-        | Tree -> (match ev_all args with
-                   | [x; `List y] ->
-                      let (y': value Tree.t list) = List.map y ~f:(fun e -> match e with `Tree t -> t | _ -> arg_error op args) in
-                      `Tree (Tree.Node (x, y'))
-                   | _ -> arg_error op args)
-        | Value -> (match ev_all args with
-                    | [`Tree (Tree.Node (x, _))] -> x
+          | Car -> (match ev_all args with
+                    | [`List (x::_)] -> x
                     | _ -> arg_error op args)
-        | Children -> (match ev_all args with
-                       | [`Tree Tree.Empty] -> `List []
-                       | [`Tree (Tree.Node (_, x))] -> `List (List.map x ~f:(fun e -> `Tree e))
-                       | _ -> arg_error op args)
-        | If -> (match args with
-                 | [ux; uy; uz] -> (match ev ctx ux with
-                                    | `Bool x -> if x then ev ctx uy
-                                                 else ev ctx uz
-                                    | _ -> arg_error op args)
-                 | _ -> arg_error op args))
+          | Cdr -> (match ev_all args with
+                    | [`List (_::xs)] -> `List xs
+                    | _ -> arg_error op args)
+          | Plus -> (match ev_all args with
+                     | [`Num x; `Num y] -> `Num (x + y)
+                     | _ -> arg_error op args)
+          | Minus -> (match ev_all args with
+                      | [`Num x; `Num y] -> `Num (x - y)
+                      | _ -> arg_error op args)
+          | Mul -> (match ev_all args with
+                    | [`Num x; `Num y] -> `Num (x * y)
+                    | _ -> arg_error op args)
+          | Div -> (match ev_all args with
+                    | [`Num x; `Num y] -> 
+                       if y = 0 then raise (RuntimeError "Divide by zero.") else `Num (x / y)
+                    | _ -> arg_error op args)
+          | Mod -> (match ev_all args with
+                    | [`Num x; `Num y] ->
+                       if y = 0 then raise (RuntimeError "Divide by zero.") else `Num (x mod y)
+                    | _ -> arg_error op args)
+          | Eq -> (match ev_all args with
+                   | [x; y] -> `Bool (x = y)
+                   | _ -> arg_error op args)
+          | Neq -> (match ev_all args with
+                    | [x; y] -> `Bool (x <> y)
+                    | _ -> arg_error op args)
+          | Lt -> (match ev_all args with
+                   | [`Num x; `Num y] -> `Bool (x < y)
+                   | _ -> arg_error op args)
+          | Leq -> (match ev_all args with
+                    | [`Num x; `Num y] -> `Bool (x <= y)
+                    | _ -> arg_error op args)
+          | Gt -> (match ev_all args with
+                   | [`Num x; `Num y] -> `Bool (x > y)
+                   | _ -> arg_error op args)
+          | Geq -> (match ev_all args with
+                    | [`Num x; `Num y] -> `Bool (x >= y)
+                    | _ -> arg_error op args)
+          | And -> (match ev_all args with
+                    | [`Bool x; `Bool y] -> `Bool (x && y)
+                    | _ -> arg_error op args)
+          | Or -> (match ev_all args with
+                   | [`Bool x; `Bool y] -> `Bool (x || y)
+                   | _ -> arg_error op args)
+          | Cons -> (match ev_all args with
+                     | [x; `List y] -> `List (x :: y)
+                     | _ -> arg_error op args)
+          | Tree -> (match ev_all args with
+                     | [x; `List y] ->
+                        let (y': value Tree.t list) = List.map y ~f:(fun e -> match e with `Tree t -> t | _ -> arg_error op args) in
+                        `Tree (Tree.Node (x, y'))
+                     | _ -> arg_error op args)
+          | Value -> (match ev_all args with
+                      | [`Tree (Tree.Node (x, _))] -> x
+                      | _ -> arg_error op args)
+          | Children -> (match ev_all args with
+                         | [`Tree Tree.Empty] -> `List []
+                         | [`Tree (Tree.Node (_, x))] -> `List (List.map x ~f:(fun e -> `Tree e))
+                         | _ -> arg_error op args)
+          | If -> (match args with
+                   | [ux; uy; uz] -> (match ev ctx lim ux with
+                                      | `Bool x -> if x then ev ctx lim uy
+                                                   else ev ctx lim uz
+                                      | _ -> arg_error op args)
+                   | _ -> arg_error op args))
   in
-  ev ctx' expr
+  ev ctx' limit expr
