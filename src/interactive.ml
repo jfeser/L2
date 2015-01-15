78c2925a5435eb2ebbb232bd1a592b3e4b04d8ea
Jack Feser <jack.feser@gmail.com>
2015-01-15 16:06:00 +0530

    Add interactive shell for synthesizer.

---
+++ src/interactive.ml
@@ -0,0 +1,44 @@
+open Core.Std
+open Printf
+
+let read_input () =
+  let input = ref [] in
+  let _done = ref false in
+  (while not !_done do
+    let line = input_line stdin in
+      if line = ""
+      then _done := true
+      else input := !input @ [line]
+  done);
+  !input
+
+let solve bk_strs example_strs =
+  begin
+    let bk = List.map bk_strs ~f:(fun (name, impl) -> name, Util.parse_expr impl) in
+    let examples = List.map example_strs ~f:Util.parse_example in
+    let start_time = Time.now () in
+    let solutions = Search.solve ~bk examples in
+    let end_time = Time.now () in
+    let solve_time = Time.diff end_time start_time in
+    let solutions_str =
+      Util.Ctx.to_alist solutions
+      |> List.map
+           ~f:(fun (name, lambda) ->
+               match lambda with
+               | `Let (_, lambda', _) ->
+                  sprintf "%s = %s" name (lambda' |> Expr.normalize |> Expr.to_string)
+               | _ -> failwith "Unexpected expression.")
+      |> String.concat ~sep:"\n"
+    in
+    printf "Solved in %s. Solutions:\n%s\n\n"
+           (Time.Span.to_short_string solve_time) solutions_str;
+    flush stdout;
+  end
+
+let () =
+  try
+    while true do
+      let input = read_input () in
+      solve [] input
+    done
+  with End_of_file -> ()
