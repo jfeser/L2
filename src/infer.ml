382508a70678f72931d11306408b17ebd337cc5e
Jack Feser <jack.feser@gmail.com>
2015-03-14 13:49:20 -0500

    Move type manipulation functions into Type module.

--- src/infer.ml
+++ src/infer.ml
@@ -7,6 +7,54 @@ open Util
 
 exception TypeError of string
 
+module Type = struct
+  type t = typ with compare, sexp
+
+  (** Return the nesting depth of this type. For example, the type
+      "int" has a nesting depth of 1, and the type "list[int]" has a
+      nesting depth of 2. *)
+  let rec nesting_depth (t: t) : int =
+    match t with
+    | Const_t _ | Var_t _ -> 1
+    | App_t (_, args) -> 1 + (max (List.map ~f:nesting_depth args))
+    | Arrow_t (args, ret) ->
+      let args_max = (max (List.map ~f:nesting_depth args)) in
+      let ret_depth = nesting_depth ret in
+      if args_max > ret_depth then args_max else ret_depth
+
+  (** Normalize a type by renaming each of its quantified type variables. *)
+  let normalize (t: t) : t =
+    let count = ref (-1) in
+    let fresh_name () = incr count; "t" ^ (Int.to_string !count) in
+    let rec norm ctx typ = match typ with
+      | Const_t _
+      | Var_t {contents = Free _} -> typ
+      | Var_t {contents = Link typ'} -> norm ctx typ'
+      | Var_t {contents = Quant name} ->
+        (match Ctx.lookup ctx name with
+         | Some name' -> Var_t (ref (Quant name'))
+         | None -> let name' = fresh_name () in
+           Ctx.update ctx name name'; Var_t (ref (Quant name')))
+      | App_t (const, args) -> App_t (const, List.map args ~f:(norm ctx))
+      | Arrow_t (args, ret) -> Arrow_t (List.map args ~f:(norm ctx), norm ctx ret)
+    in
+    norm (Ctx.empty ()) t
+
+  let rec to_string (t: t) : string =
+    let tlist_str typs =
+      typs |> List.map ~f:to_string |> String.concat ~sep:", "
+    in
+    match t with
+    | Const_t Num_t -> "num"
+    | Const_t Bool_t -> "bool"
+    | Var_t {contents = Free (id, _)} -> "ft" ^ (Int.to_string id)
+    | Var_t {contents = Quant name} -> name
+    | Var_t {contents = Link typ'} -> to_string typ'
+    | App_t (id, args) -> sprintf "%s[%s]" id (tlist_str args)
+    | Arrow_t ([arg], ret) -> sprintf "(%s -> %s)" (to_string arg) (to_string ret)
+    | Arrow_t (args, ret) -> sprintf "((%s) -> %s)" (tlist_str args) (to_string ret)
+end
+
 module TypedExpr = struct
   type t =
     | Num of int * typ
@@ -170,22 +218,7 @@ end
 
 let fresh_free level = Var_t (ref (Free (Fresh.int (), level)))
 
-let normalize typ =
-  let count = ref (-1) in
-  let fresh_name () = incr count; "t" ^ (Int.to_string !count) in
-  let rec norm ctx typ = match typ with
-    | Const_t _
-    | Var_t {contents = Free _} -> typ
-    | Var_t {contents = Link typ'} -> norm ctx typ'
-    | Var_t {contents = Quant name} ->
-       (match Ctx.lookup ctx name with
-        | Some name' -> Var_t (ref (Quant name'))
-        | None -> let name' = fresh_name () in
-                  Ctx.update ctx name name'; Var_t (ref (Quant name')))
-    | App_t (const, args) -> App_t (const, List.map args ~f:(norm ctx))
-    | Arrow_t (args, ret) -> Arrow_t (List.map args ~f:(norm ctx), norm ctx ret)
-  in
-  norm (Ctx.empty ()) typ
+let normalize = Type.normalize
 
 let occurs id level typ =
   let error msg =
