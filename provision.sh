07370c78232ab23a90a6701463211f87d9fec471
Jack Feser <jack.feser@gmail.com>
2015-03-27 13:14:11 -0500

    Add Bolt to provision script.

--- provision.sh
+++ provision.sh
@@ -8,13 +8,13 @@ apt-get update
 apt-get -y upgrade
 
 # Install ocaml packages.
-apt-get -y install ocaml-nox ocaml-native-compilers ocaml-findlib opam m4 gettext zlib1g-dev libcurl4-openssl-dev autoconf
+apt-get -y install ocaml-nox ocaml-native-compilers ocaml-findlib opam m4 gettext zlib1g-dev libcurl4-openssl-dev autoconf camlp4-extra
 
 # Install opam and packages.
 mkdir -p $OPAM_ROOT
 opam init --auto-setup --yes --root $OPAM_ROOT
 eval "$(opam config env --root $OPAM_ROOT)"
-opam install -y core menhir utop
+opam install -y core menhir utop bolt
 
 # Set permissions so that the user can install using opam.
 chown -R vagrant:vagrant $OPAM_ROOT
