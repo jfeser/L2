7872b5fcd28012777d7e9ec51d02ff7824ea1e9f
Jack Feser <jack.feser@gmail.com>
2015-06-23 21:49:48 -0700

    Update deduction interface.

--- src/deduction.mli
+++ src/deduction.mli
@@ -1,9 +1,10 @@
-open Core.Std
-open Collections
-
 (** Write the performance counters to the log at level INFO. *)
 val log_summary : unit -> unit
 
+val assert_of_expr : Z3.context -> Infer.TypedExpr.t -> Z3.Expr.expr
+
+val z3_to_string : Z3.Expr.expr -> string
+                                                          
 val infer_length_constraint : Z3.context -> Ast.example list -> Z3.Expr.expr
 
 val memoized_check_constraints :
