fdc1d71d43df89dace25388957a174c9ef40ffd3
Jack Feser <jack.feser@gmail.com>
2016-07-17 19:13:00 +0100

    Add an interface for Eval.

---
+++ src/l2/eval.mli
@@ -0,0 +1,13 @@
+open Core.Std
+open Collections
+
+exception RuntimeError of Error.t
+exception HitRecursionLimit
+
+val eval : ?recursion_limit:int -> Value.t Ctx.t -> Expr.t -> Value.t
+                                                                
+val partial_eval :
+  ?recursion_limit:int
+  -> ?ctx:ExprValue.t Ctx.t
+  -> ExprValue.t
+  -> ExprValue.t
